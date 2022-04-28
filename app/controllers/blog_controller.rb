649ef3a4d9aa0b378dbd329567c83bb4e3d23743
Velasqj8 <jv_619@yahoo.com>
2022-04-28 11:55:58 -0700

    Built app & working on routes

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,25 @@
 class BlogController < ApplicationController
   def index
-    @blog = Blog.all
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+    if @blog.valid?
+      redirect_to blog_path
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get 'blog' => 'blog#index', as: 'blogs'
+  get 'index' => 'blog#index', as: 'blogs'
+  get 'blog/new' => 'blog#new', as: 'new_blog'
+  get 'blog/:id' => 'blog#show', as: 'blog'
+  post 'blog' => 'blog#create'
 end
